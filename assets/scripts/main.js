64290178461bd1e7df2179061265a27073411049
Hong <hojin@ucsd.edu>
2022-11-09 11:24:58 -0800

    Finished writing the expose part, bugs not fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,6 +68,10 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  let recipes = localStorage.getItem('recipes');
+  if(recipes != NaN){
+	return recipes;
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -78,28 +82,46 @@ async function getRecipes() {
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+  const arrRecipes = [];
+  arrRecipes = new Promise(async (resolve, reject) => {
+	// A4-A11 will all be *inside* the callback function we passed to the Promise
+	// we're returning
+	/**************************/
+	// A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+	//            declared above
+	// A5. TODO - Since we are going to be dealing with asynchronous code, create
+	//            a try / catch block. A6-A9 will be in the try portion, A10-A11
+	//            will be in the catch portion.
+	for(const url in RECIPE_URLS){
+		try {
+			// A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+			//            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+			//            you must either use "await fetch(...)" or "fetch.then(...)". This
+			//            function is using the async keyword so we recommend "await"
+			const fetchURL = await fetch(url);
+			// A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+			//            NOTE: .json() is ALSO asynchronous, so you will need to use
+			//            "await" again
+			const recipe = await fetchURL.json();
+			// A8. TODO - Add the new recipe to the recipes array
+			arrRecipes.push(recipe);
+			// A9. TODO - Check to see if you have finished retrieving all of the recipes,
+			//            if you have, then save the recipes to storage using the function
+			//            we have provided. Then, pass the recipes array to the Promise's
+			//            resolve() method.
+			if(url === 'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json'){
+				saveRecipesToStorage(arrRecipes);
+				resolve(arrRecipes);
+			}
+		} catch (error) {
+			// A10. TODO - Log any errors from catch using console.error
+			console.error(error);
+			// A11. TODO - Pass any errors to the Promise's reject() function
+			reject(new Error(error));
+		}
+	}
+  });
+
 }
 
 /**
